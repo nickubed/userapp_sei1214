525cafaf42d450b87a5c4331df70ab63b7d3dea0
nickubed <nhpquandt@gmail.com>
2021-01-15 10:40:12 -0800

    updated with 1:M

--- index.js
+++ index.js
@@ -52,3 +52,55 @@ const db = require('./models')
 //     console.log(rowsDeleted)
 //     process.exit()
 // })
+
+// createModel, getModels, setModel, and addModel
+
+// db.user.findOne({where: {firstName: 'Weston'}})
+// .then(user => {
+//     console.log("Adding a pet to this user: ", user.firstName)
+//     user.createPet({
+//         name: 'Porkroll',
+//         species: 'Cat'
+//     }).then(pet => {
+//         console.log(pet)
+//         process.exit()
+//     })
+// })
+
+// db.user.findOne({
+//     where: {firstName: 'Nick'}
+// }).then(user => {
+//     user.getPets().then(pets => {
+//         console.log(`${user.firstName}'s pets: `)
+//         console.log(pets)
+//         process.exit()
+//     })
+// })
+
+// db.pet.findOrCreate({
+//     where: {
+//         name: 'Oscar',
+//         species: 'Cat'
+//     },
+//     defaults: {
+//         description: 'Old oreo-colored cat, likes food and not much else.'
+//     }
+// }).then(([pet, wasCreated]) => {
+//     db.user.findOne({where: {firstName: 'Nick'}})
+//     .then(coolDude => {
+//         coolDude.addPet(pet)
+//         console.log(`User ${coolDude.firstName} is the owner of ${pet.name}`)
+//     })
+// })
+
+db.user.findAll({
+    include: [db.pet]
+}).then(users => {
+    users.forEach(user => {
+        console.log(`${user.firstName}'s pets: `)
+        user.pets.forEach(pet => {
+            console.log(pet.name)
+        })
+    })
+    process.exit()
+})
